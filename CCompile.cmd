39c5bb7c6cd31bdf90da241e01811b0ddca833ab
Kat <33645731+Katistic@users.noreply.github.com>
2019-12-12 02:21:43 +1100

    Update CCompile.cmd

--- CCompile.cmd
+++ CCompile.cmd
@@ -1,1 +1,1 @@
-pyinstaller -y -F --clean -n SimplYoutube "./SimplYoutube.py"
+pyinstaller -y -F -n SimplYoutube --add-data "./ffmpeg";"ffmpeg/" --add-data "C:/Users/Princess/AppData/Local/Programs/Python/Python37-32/Lib/site-packages/soundcard";"soundcard/" "./SimplYoutube.py"
